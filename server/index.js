2f9b89b88ea6a6e31d8f24e17b4be932953a80cf
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-27 13:55:25 +0300

    Mongo db ile bağlantı kuruldu

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cokkieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const db = require("./config/db.js");
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cokkieParser());
+
+db();
 
 const PORT = 5000;
 
